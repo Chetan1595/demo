289fbd7f137732c67600faeea92accaf7993379d
Chetan1595 <cu.18bcs1445@gmail.com>
2022-04-27 19:20:17 +0530

    change

---
+++ Change.java
@@ -0,0 +1,59 @@
+package hit;
+
+class Returns{
+	private int roll;
+	private String name;
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Returns(String name, int roll) {
+		this.name = name;
+		this.roll = roll;
+	}
+	
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setRoll(int roll) {
+//		this.roll = roll;
+//	}
+//	public int getRoll() {
+//		return roll;
+//	}
+	public String abcd() {
+		return name+" "+roll;
+	}
+	public String toString() {
+		return name+name+" "+roll+roll;
+	}
+	
+}
+
+public class Change {
+
+	public static void main(String[] args) {
+		Returns ret = new Returns("car",99);
+		System.out.println(ret.abcd());
+		ret.setName("Chetan");
+		System.out.println(ret.getName());
+		ret.setRoll(10);
+		System.out.println(ret.getRoll());
+		System.out.println(ret);
+		
+
+	}
+
+}
